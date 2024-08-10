e442f0d8076871759af507e7998fbc678d15490e
Bharat Chaudhary <bhrt.7856@gmail.com>
2024-08-10 15:30:12 +0545

    new ch

--- route/auth.js
+++ route/auth.js
@@ -3,7 +3,7 @@ const { signup, login, deleteUser } = require("../controller/auth");
 const router = express.Router();
 const cors = require("cors");
 
-router.post("/signup", cors(), signup);
+router.post("/signup", signup);
 router.post("/login", login);
 
 module.exports = router;
