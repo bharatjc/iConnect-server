1d9cf7d22cc012104e2bc80ee811207f3066ee8f
Bharat Chaudhary <bhrt.7856@gmail.com>
2024-08-11 21:03:56 +0545

    sdfd

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://bharat:2h1r1tjung@bharat.ysbba.mongodb.net/?retryWrites=true&w=majority&appName=Bharat"
+    "mongodb+srv://Bharat:2h1r1tjung@cluster0.t2vg6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => console.log("Connected!"));
