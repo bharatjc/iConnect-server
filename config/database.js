e3bfd8d6b09bef81c27f455b8b461a3b56ec0ca1
Bharat Chaudhary <bhrt.7856@gmail.com>
2024-08-12 08:31:13 +0545

    ltch

--- config/database.js
+++ config/database.js
@@ -1,8 +1,4 @@
 const mongoose = require("mongoose");
-// require("dotenv").config();
+require("dotenv").config();
 
-mongoose
-  .connect(
-    "mongodb+srv://bharat:2h1r1tjung@bharat.ysbba.mongodb.net/iConnect?retryWrites=true&w=majority&appName=Bharat"
-  )
-  .then(() => console.log("Connected!"));
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("Connected!"));
